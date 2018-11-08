afbabde78f26ab81d3814a3ac27df43604581883
Milou van Casteren <Milou@wcw-stud-145-109-63-154.wireless.uva.nl>
2018-11-08 13:11:33 +0100

    coordinaten

---
+++ amstelhaege.py
@@ -0,0 +1,18 @@
+# Milou van Casteren
+# minor programmeren
+# Heuristieken
+# Amstelhaege
+
+
+# grid maken
+
+
+lengte = 32
+breedte = 36
+print("test")
+
+for y in reversed(range(lengte + 1)):
+    for x in range(breedte + 1):
+        coordinaat = f"({x},{y})"
+        print(coordinaat, end="")
+    print()
