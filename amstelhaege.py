b3ff0013f6149a82c62112ca1ab28edc8023c858
Maria Daan <mariadaan@gmail.com>
2018-11-08 13:27:51 +0100

    mijn eigen naam er ook bij

--- amstelhaege.py
+++ amstelhaege.py
@@ -1,4 +1,4 @@
-# Milou van Casteren
+# Milou van Casteren en Maria Daan
 # minor programmeren
 # Heuristieken
 # Amstelhaege
@@ -10,6 +10,7 @@
 lengte = 32
 breedte = 36
 print("test")
+print("Maria")
 
 for y in reversed(range(lengte + 1)):
     for x in range(breedte + 1):
