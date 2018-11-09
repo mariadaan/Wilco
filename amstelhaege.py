2f25767ce5f4fffb6d46504dec16c6b75dbe4548
Ad Ruigrok van der Werve <adruigrokvanderwerve@wcw-stud-145-109-10-233.wireless.uva.nl>
2018-11-09 14:41:03 +0100

    grid

--- amstelhaege.py
+++ amstelhaege.py
@@ -6,14 +6,46 @@
 
 # grid maken
 
+class Grid():
 
-lengte = 32
-breedte = 36
-print("test")
+    def __init__(self, lengte, breedte):
+        self.lengte = lengte
+        self.breedte = breedte
 
-for y in reversed(range(lengte + 1)):
-    for x in range(breedte + 1):
-        coordinaat = f"({x},{y})"
-        print(coordinaat, end="")
-    print()
+
+    def coordinaat(self):
+
+        for y in reversed(range(self.lengte + 1)):
+            for x in range(self.breedte + 1):
+                if len(str(y)) == 1:
+                    coordinaat = f"({x},0{y})"
+                    print(coordinaat, end="")
+                else:
+                    coordinaat = f"({x},{y})"
+                    print(coordinaat, end="")
+            print()
+
+class Huis():
+
+    def __init__(self):
+        # self.naam = naam
+        self.oppervlakte = oppervlakte
+        # self.vrijstand = vrijstand
+        # self.prijs = pr ijs
+        # self.meerwaarde = meerwaarde
+
+    def huis(self)
+
+
+
+
+if __name__ == "__main__":
+    g = Grid(32, 36)
+    g.coordinaat()
+
+
+
+
+
+
 # Wilco
