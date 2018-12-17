43459e560e20d303c7c9d62a9f27051f2ec2e50a
Maria Daan <mariadaan@gmail.com>
2018-12-18 00:03:06 +0100

    ff in een mappie

--- code/house.py
+++ code/house.py
@@ -1,8 +1,8 @@
 class House(object):
-"""
-This is the House class. In this object, all features of each house type
-are defined.
-"""
+    """
+    This is the House class. In this object, all features of each house type
+    are defined.
+    """
     def __init__(self, id, name, length, width, price, min_space, value_increase):
         self.id = id
         self.name = name
